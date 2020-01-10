e9dd4ec2ccf00aa5c3f74bb824bc843103de0a59
Caroline Artz <ceartz@gmail.com>
2020-01-10 16:29:25 -0600

    Initial commit.

---
+++ gatsby-config.js
@@ -0,0 +1,82 @@
+/* eslint-disable @typescript-eslint/camelcase */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path")
+
+module.exports = {
+  siteMetadata: {
+    title: `Caroline Artz - Personal Website`,
+    description: `All About Caroline Artz`,
+    author: `ceartz@gmail.com`,
+  },
+  plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
+    // TODO: disable unneded features when ready to deploy.
+    `gatsby-plugin-lodash`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `caroline-artz`,
+        short_name: `carolineartz`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/assets/images/favicon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        allowNamespaces: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-eslint`,
+      options: {
+        test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: [`develop`],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-svg`,
+      options: {
+        rule: {
+          include: path.resolve(__dirname, `src/assets/svg`),
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-root-import`,
+      options: {
+        components: path.join(__dirname, `src/components`),
+        pages: path.join(__dirname, `src/pages`),
+        utils: path.join(__dirname, `src/utils`),
+        assets: path.join(__dirname, `src/assets`),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/AppLayout.tsx`),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          // TODO: remove ones I don't actually use.
+          `Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i`,
+        ],
+        display: `swap`,
+      },
+    },
+  ],
+}
